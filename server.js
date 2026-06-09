3c6b94a79cb515007e4aaa5aebdd36f0dc7e1073
Austine Ouma <austinouma905@gmail.com>
2026-06-09 10:32:07 +0300

    fix: allow x-admin-secret header in CORS

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-admin-secret']
 }));
 
 app.use(express.json());
